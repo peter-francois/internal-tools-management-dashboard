8a452ca6642f65df5b1f5c27808a491f6ff5931a
Peter Francois <peter.francois237@hotmail.com>
2026-03-10 16:55:54 +0100

    feat: create react router

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Routes>        </Routes>
+      </main>
+    </>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import Header from "./components/header/Header";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Header />
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
   </StrictMode>,
 );
