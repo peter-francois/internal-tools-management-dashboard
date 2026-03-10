29615687bca827c5ef97f3a948000e110395322e
Peter Francois <peter.francois237@hotmail.com>
2026-03-10 18:07:37 +0100

    style: fix header style to match mockup

--- src/components/header/ZapLogo.tsx
+++ src/components/header/ZapLogo.tsx
@@ -3,7 +3,7 @@ import { Zap } from "lucide-react";
 const ZapLogo = () => {
   return (
     <div
-      className="flex h-7 w-7 items-center justify-around rounded-lg bg-linear-to-br from-blue-400  from-5% via-indigo-500 via-30% to-purple-600"
+      className="flex h-8 w-8 items-center justify-around rounded-lg bg-linear-to-br from-blue-400  from-5% via-indigo-500 via-30% to-purple-600"
       aria-hidden="true"
     >
       <Zap size={18} className="text-white " strokeWidth={2} />
