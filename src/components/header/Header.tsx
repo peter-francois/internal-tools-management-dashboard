62280b2c9a8840365c3dc8f8e0fb90f8144fc909
peter-francois <contact@peterfrancois.dev>
2026-03-10 14:22:32 +0100

    feat: create desktop navigation bar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,12 @@
-import LogoAndCompanyName from "./Logo";
+import DesktopNav from "./DesktopNav";
+import LogoAndCompanyName from "./LogoAndCompanyName";
 
 const Header = () => {
   return (
     <header className="sticky top-0 w-full z-40 border-b border-gray-200 bg-white/90">
       <div className="mx-auto flex h-14 items-center gap-4 px-6">
         <LogoAndCompanyName />
+        <DesktopNav />
       </div>
     </header>
   );
