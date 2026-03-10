5b8d2dbf93bf1d28b792265b1a6b161d8a25cbd6
Peter Francois <peter.francois237@hotmail.com>
2026-03-10 12:37:48 +0100

    feat: create ZapLogo component

---
+++ src/components/header/ZapLogo.tsx
@@ -0,0 +1,11 @@
+import { Zap } from "lucide-react";
+
+const ZapLogo = () => {
+  return (
+    <div className="flex h-8 w-8 items-center justify-around rounded-lg bg-linear-to-br from-blue-400  from-5% via-indigo-500 via-30% to-purple-600">
+      <Zap size={18} className="text-white " strokeWidth={2} />
+    </div>
+  );
+};
+
+export default ZapLogo;
