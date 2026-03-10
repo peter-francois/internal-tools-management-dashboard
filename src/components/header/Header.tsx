af64551f8e2e1073fdfb360e4219e38c2f5f37e1
Peter Francois <peter.francois237@hotmail.com>
2026-03-10 13:31:42 +0100

    feat: create header with logo and company name

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import LogoAndCompanyName from "./Logo";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 w-full z-40 border-b border-gray-200 bg-white/90">
+      <div className="mx-auto flex h-14 items-center gap-4 px-6">
+        <LogoAndCompanyName />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
