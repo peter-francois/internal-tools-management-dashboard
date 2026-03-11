cb1e43bb7c9d2639762607512e2acad9f86f8c15
Peter Francois <peter.francois237@hotmail.com>
2026-03-11 15:22:02 +0100

    feat: create mock and constant for statCard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,23 +1,23 @@
-import { TrendingUp } from "lucide-react";
 import StatCard from "../components/ui/StatCard";
+import { STAT_CARDS_MOCK } from "../data/statCard";
 import { NAV_ITEMS } from "../types/contants/navigation";
+import { STAT_CARDS_VISUAL } from "../types/contants/statCards";
 
 const Dashboard = () => {
   return (
     <>
       <h1>{NAV_ITEMS.dashboard.title}</h1>
       <p>{NAV_ITEMS.dashboard.description}</p>
-      <StatCard
-        label="Monthly Budget"
-        icon={<TrendingUp size={16} />}
-        value="€28,750"
-        valueSecondary="/€30k"
-        trend="+12%"
-        trendVariant="positive"
-        fromColor="from-emerald-500"
-        viaColor="via-emerald-500"
-        toColor="to-teal-600"
-      />
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 mt-6">
+        {Object.keys(STAT_CARDS_VISUAL).map((key) => (
+          <StatCard
+            key={key}
+            {...STAT_CARDS_VISUAL[key as keyof typeof STAT_CARDS_VISUAL]}
+            {...STAT_CARDS_MOCK[key as keyof typeof STAT_CARDS_MOCK]}
+          />
+        ))}
+      </div>
     </>
   );
 };
