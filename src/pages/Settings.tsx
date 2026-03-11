3ad64d0ae8429cfbc309f7dac1b10eeeeb72b900
Peter Francois <peter.francois237@hotmail.com>
2026-03-11 11:31:08 +0100

    feat: create initial settings page

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,10 @@
+const Settings = () => {
+  return (
+    <>
+      <h1>Settings</h1>
+      <p>Manage your account, preferences and organization settings</p>
+    </>
+  );
+};
+
+export default Settings;
