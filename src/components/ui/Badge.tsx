95cf4999f80ea5a14f51ba7b548767bcc484c276
Peter Francois <peter.francois237@hotmail.com>
2026-03-11 15:58:20 +0100

    feat: create badge and use in stat card

---
+++ src/components/ui/Badge.tsx
@@ -0,0 +1,14 @@
+interface BadgeProps {
+  label: string;
+trendClass: string
+}
+
+export default function Badge({ label, trendClass }: BadgeProps) {
+  return (
+    <span
+      className={`inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-medium text-white ${trendClass}`}
+    >
+      {label}
+    </span>
+  );
+}
