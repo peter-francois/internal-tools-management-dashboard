7af4466f4053db26f899110e6094efe6e316e894
Peter Francois <peter.francois237@hotmail.com>
2026-03-11 11:24:21 +0100

    feat: create initial tools page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import { NAV_ITEMS } from "./types/contants/navigation";
 import AppLayout from "./layouts/AppLayout";
+import Tools from "./pages/Tools";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path={NAV_ITEMS.dashboard.path} element={<AppLayout />}>
           <Route index element={<Dashboard />} />
+          <Route index element={<Tools />} />          
         </Route>
       </Routes>
     </>
---
+++ src/pages/Tools.tsx
@@ -0,0 +1,10 @@
+const Tools = () => {
+  return (
+    <>
+      <h1>Tools</h1>
+      <p>Manage and monitor all your organization's software tools</p>
+    </>
+  );
+};
+
+export default Tools;
