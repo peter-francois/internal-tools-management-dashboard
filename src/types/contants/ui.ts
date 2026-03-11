eb4a45e1878abd8f0576eb4cc49a38dac0f2917f
Peter Francois <peter.francois237@hotmail.com>
2026-03-11 12:29:49 +0100

    fix: remove go back button in not found page

--- src/types/contants/ui.ts
+++ src/types/contants/ui.ts
@@ -2,6 +2,5 @@ export const NOT_FOUND = {
   code: "404",
   title: "Page not found",
   description: "The page you're looking for doesn't exist or has been moved.",
-  goBack: "Go back",
   backToDashboard: "Back to Dashboard",
 } as const;
