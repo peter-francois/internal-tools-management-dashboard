16a80395c5ad20b925289937bedbcaec135dcb0b
peter-francois <contact@peterfrancois.dev>
2026-03-12 13:55:40 +0100

    feat: add tools query params

--- src/hooks/useToolService.ts
+++ src/hooks/useToolService.ts
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { getToolsApi } from "../api/tool.api";
 import { FIVE_MINUTE_IN_MS } from "../types/contants/time.constant";
+import type { ToolsQueryParams } from "../types/interfaces/tool.interface";
 
-export const useTools = () =>
+export const useTools = (params?: ToolsQueryParams) =>
   useQuery({
-    queryKey: ["tools"],
-    queryFn: getToolsApi,
+    queryKey: ["tools", params],
+    queryFn: () => getToolsApi(params),
     staleTime: FIVE_MINUTE_IN_MS,
   });
--- src/types/interfaces/tool.interface.ts
+++ src/types/interfaces/tool.interface.ts
@@ -14,4 +14,15 @@ export type SortField = keyof Pick<
   Tool,
   "name" | "department" | "users" | "monthlyCost" | "status"
 >;
+
 export type SortDir = "asc" | "desc";
+
+export type sortType = "updated_at" | "monthly_cost" | "name";
+
+export interface ToolsQueryParams {
+  _sort?: sortType
+  _order?: SortDir
+  _limit?: number;
+  _page?: number;
+  _embed?: "user_tools";
+}
