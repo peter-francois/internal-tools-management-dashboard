5748192fb1e2d000330d7ac70c5cbef552f9b3ab
peter-francois <contact@peterfrancois.dev>
2026-03-12 09:25:48 +0100

    feat: create tool interface and mock data

---
+++ src/types/contants/toolStatus.ts
@@ -0,0 +1,22 @@
+export const TOOL_STATUS = {
+  active: {
+    label: "Active",
+    fromColor: "from-emerald-400",
+    viaColor: "via-emerald-500",
+    toColor: "to-teal-600",
+  },
+  expiring: {
+    label: "Expiring",
+    fromColor: "from-orange-400",
+    viaColor: "via-orange-500",
+    toColor: "to-amber-600",
+  },
+  unused: {
+    label: "Unused",
+    fromColor: "from-red-400",
+    viaColor: "via-red-500",
+    toColor: "to-rose-600",
+  },
+} as const;
+
+export type ToolStatus = keyof typeof TOOL_STATUS;
