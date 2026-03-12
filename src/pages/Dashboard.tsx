7ba5b03be3bf4b4138f9a6438aa7c72b4a6e5640
peter-francois <contact@peterfrancois.dev>
2026-03-12 08:43:56 +0100

    refactor: create stat card list component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,5 @@
-import StatCard from "../components/ui/StatCard";
-import { STAT_CARDS_MOCK } from "../data/statCard";
+import StatCardList from "../components/ui/StatCardList";
 import { NAV_ITEMS } from "../types/contants/navigation";
-import { STAT_CARDS_VISUAL } from "../types/contants/statCards";
 
 const Dashboard = () => {
   return (
@@ -9,15 +7,7 @@ const Dashboard = () => {
       <h1>{NAV_ITEMS.dashboard.title}</h1>
       <p>{NAV_ITEMS.dashboard.description}</p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 mt-6">
-        {Object.keys(STAT_CARDS_VISUAL).map((key) => (
-          <StatCard
-            key={key}
-            {...STAT_CARDS_VISUAL[key as keyof typeof STAT_CARDS_VISUAL]}
-            {...STAT_CARDS_MOCK[key as keyof typeof STAT_CARDS_MOCK]}
-          />
-        ))}
-      </div>
+      <StatCardList />
     </>
   );
 };
