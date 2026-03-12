73acf24965e12d31ec0e0e066592d25da76db45d
peter-francois <contact@peterfrancois.dev>
2026-03-12 10:51:51 +0100

    feat: add query client using tanstack query

---
+++ src/utils/queryClients.ts
@@ -0,0 +1,20 @@
+import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
+import { QueryClient } from "@tanstack/react-query";
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
+const localStoragePersister = createAsyncStoragePersister({
+  storage: window.localStorage,
+});
+
+persistQueryClient({
+  queryClient,
+  persister: localStoragePersister,
+});
