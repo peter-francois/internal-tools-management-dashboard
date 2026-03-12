20dc5498c3c291013372993c816c7570143bab3d
Peter Francois <peter.francois237@hotmail.com>
2026-03-12 13:55:40 +0100

    feat: add tools query params

--- src/api/tool.api.ts
+++ src/api/tool.api.ts
@@ -1,10 +1,10 @@
 import type { AxiosInstance } from "axios";
 import { axiosClient } from "../utils/axiosClient";
-import type { Tool } from "../types/interfaces/tool.interface";
+import type { Tool, ToolsQueryParams } from "../types/interfaces/tool.interface";
 
 const api: AxiosInstance = axiosClient();
 
-export const getToolsApi = async (): Promise<Tool[]> => {
-  const { data } = await api.get<Tool[]>("tools");
+export const getToolsApi = async (params?: ToolsQueryParams): Promise<Tool[]> => {
+  const { data } = await api.get<Tool[]>("tools", { params });
   return data;
 };
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,31 @@
 import RecentToolsTable from "../components/dashboard/RecentToolsTable";
+import ErrorPage from "../components/ui/ErrorPage";
 import StatCardList from "../components/ui/StatCardList";
 import { useTools } from "../hooks/useToolService";
+import { ERROR_PAGES } from "../types/contants/errorPages.constant";
 import { NAV_ITEMS } from "../types/contants/navigation.constant";
 
 const Dashboard = () => {
-  const { data, isLoading } = useTools();
+  const { data, isLoading, isError, error } = useTools({ _sort: "updated_at", _order: "desc", _limit: 8 });
   console.log("🚀 ~ Dashboard.tsx:9 ~ Dashboard ~ data:", data);
+  if (isLoading) return;
+  if (isError)
+    return (
+      <ErrorPage
+        code={ERROR_PAGES.serverError.code}
+        title={ERROR_PAGES.serverError.title}
+        description={error.message}
+        action={ERROR_PAGES.serverError.action}
+      />
+    );
+  if (!data)
+    return (
+      <ErrorPage
+        code={ERROR_PAGES.noData.code}
+        title={ERROR_PAGES.noData.title}
+        description={ERROR_PAGES.noData.description}
+      />
+    );
 
   return (
     <>
