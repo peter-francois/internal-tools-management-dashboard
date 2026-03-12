27cbae6268d6d06d10f51ffec803c6c31657372a
Peter Francois <peter.francois237@hotmail.com>
2026-03-12 15:29:44 +0100

    feat: add analitics fetching

--- src/components/ui/StatCardList.tsx
+++ src/components/ui/StatCardList.tsx
@@ -1,17 +1,34 @@
-
-import { STAT_CARDS_MOCK } from "../../data/statCard.mock";
-import { STAT_CARDS_VISUAL } from "../../types/contants/statCards.constant";
+import { useAnalitics } from "../../hooks/useAnaliticsService";
+import { ERROR_PAGES } from "../../types/contants/errorPages.constant";
+import { mapAnaliticsToStatCards } from "../../utils/mapAnaliticsToStatCards";
+import ErrorPage from "./ErrorPage";
 import StatCard from "./StatCard";
 
 const StatCardList = () => {
+  const { data, isLoading, isError, error } = useAnalitics();
+  if (isLoading) return;
+  if (isError)
+    return (
+      <ErrorPage
+        code={ERROR_PAGES.serverError.code}
+        title={ERROR_PAGES.serverError.title}
+        description={error.message}
+        action={ERROR_PAGES.serverError.action}
+      />
+    );
+  if (!data)
+    return (
+      <ErrorPage
+        code={ERROR_PAGES.noData.code}
+        title={ERROR_PAGES.noData.title}
+        description={ERROR_PAGES.noData.description}
+      />
+    );
+  const cards = mapAnaliticsToStatCards(data, "51", "45");
   return (
     <div className="mt-6 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
-      {Object.keys(STAT_CARDS_VISUAL).map((key) => (
-        <StatCard
-          key={key}
-          {...STAT_CARDS_VISUAL[key as keyof typeof STAT_CARDS_VISUAL]}
-          {...STAT_CARDS_MOCK[key as keyof typeof STAT_CARDS_MOCK]}
-        />
+      {Object.entries(cards).map(([key, card]) => (
+        <StatCard key={key} {...card} />
       ))}
     </div>
   );
