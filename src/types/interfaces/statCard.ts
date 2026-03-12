0e8b39747985f2a10ae24c85c4f87de24c11582d
peter-francois <contact@peterfrancois.dev>
2026-03-12 08:42:54 +0100

    fix: remove trendVariant since the gradient is always the same in the mockup

--- src/types/interfaces/statCard.ts
+++ src/types/interfaces/statCard.ts
@@ -1,12 +1,9 @@
-export type TrendVariant = "positive" | "negative";
-
 export interface StatCardProps {
   label: string;
   icon: React.ReactNode;
   value: string;
   valueSecondary?: string;
   trend: string;
-  trendVariant: TrendVariant;
   fromColor: string;
   viaColor: string;
   toColor: string;
